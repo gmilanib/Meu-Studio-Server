7bf07e5d97f21d5ff3348ec14e7dc6968e179cd8
GMBFedora <gabrielmilanib@gmail.com>
2026-05-27 21:51:06 -0300

    Initial commit

---
+++ src/main/java/com/example/meustudio/MeuStudioApplication.java
@@ -0,0 +1,15 @@
+package com.example.meustudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuStudioApplication.class, args);
+
+
+   }
+
+}
---
+++ src/test/java/com/example/meustudio/MeuStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meustudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
