fb045c2fcff9f2c15b0ee263c390d56589309290
GMBFedora <gabrielmilanib@gmail.com>
2026-07-30 08:53:45 -0300

    Begin login auth

--- src/main/java/com/example/meustudio/config/SecurityConfig.java
+++ src/main/java/com/example/meustudio/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.meustudio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,9 +13,9 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) {
                 http
+                                .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(authz -> authz
-                                                .anyRequest().authenticated())
-                                .httpBasic(Customizer.withDefaults());
+                                                .anyRequest().permitAll());
                 return http.build();
         }
 }
