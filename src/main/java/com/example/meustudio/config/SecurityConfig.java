ef993f5c6c1db760eefe3ff48d2e1d9c29b80ec1
GMBFedora <gabrielmilanib@gmail.com>
2026-07-30 08:32:10 -0300

    Changes to vscode

---
+++ src/main/java/com/example/meustudio/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.meustudio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) {
+                http
+                                .authorizeHttpRequests(authz -> authz
+                                                .anyRequest().authenticated())
+                                .httpBasic(Customizer.withDefaults());
+                return http.build();
+        }
+}
